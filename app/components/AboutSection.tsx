e8ca743e56fedeb24e61150e65473a370680009b
BenjaminLi <25benjaminli@gmail.com>
2025-12-26 23:06:46 +0800

    add carousel, misc updates

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -1,12 +1,71 @@
-import Image from 'next/image';
+import Carousel from './Carousel';
+
+const images = [
+	{ 
+		src: "/photo_album/rainier.png", 
+		alt: "Rainier", 
+		caption: "Hiking near Mount Rainier, WA.",
+		width: 500,
+		height: 500
+	},
+	{ 
+		src: "/photo_album/rainier_night.png", 
+		alt: "Rainier (night)", 
+		caption: "Loved Rainier so much had to include another night flick here.",
+		width: 500,
+		height: 500
+	},
+	{ 
+		src: "/photo_album/cascades.png", 
+		alt: "North Cascades", 
+		caption: "North Cascades National Park, some more breathtaking views.",
+		width: 500,
+		height: 500
+	},
+	{ 
+		src: "/photo_album/death_valley.png", 
+		alt: "Death Valley", 
+		caption: "Enjoyed getting cooked alive at Death Valley!",
+		width: 500,
+		height: 500
+	},
+	{ 
+		src: "/photo_album/orange_reservoir.png", 
+		alt: "Orange Reservoir", 
+		caption: "Orange reservoir, love to go here often when I'm in NJ.",
+		width: 500,
+		height: 500
+	},
+	{ 
+		src: "/photo_album/cornell_sunset.png", 
+		alt: "Cornell Sunset", 
+		caption: "Cornell sunset, amazing :O",
+		width: 500,
+		height: 500
+	},
+	{ 
+		src: "/photo_album/rock.png", 
+		alt: "Rock", 
+		caption: "A special rock. If you know, you know ;)",
+		width: 500,
+		height: 500
+	},
+	{ 
+		src: "/photo_album/bouldering.png", 
+		alt: "Bouldering", 
+		caption: "Bouldering at local climbing gym!",
+		width: 500,
+		height: 500
+	},
+];
 
 const AboutSection = () => {
 	return (
 		<div className="defaultContainer">
 			<div className="space-y-6">
 				<p>
-					Hello! My name is Benjamin and I'm a first-year student at Cornell University, originally from Millburn, New Jersey. 
-					I'm interested in solving socially important problems (e.g. in healthcare and the environment) with computing, particularly with AI and computer vision.
+					Hello 👋! My name is Benjamin and I'm a first-year student at Cornell University, originally from Millburn, New Jersey. 
+					I am a big AI and computer vision enthusiast especially for areas like healthcare and the environment.
 					
 				</p>
 				<p>
@@ -15,35 +74,7 @@ const AboutSection = () => {
 					I also enjoy hiking, rock climbing, and learning about geography — my favorite geological feature is <a href = "https://en.wikipedia.org/wiki/Zhangjiajie_National_Forest_Park" target = "_blank" rel="noopener noreferrer">Zhangjiajie National Forest Park</a>. Here are some photos from places I've been!
 				</p>
 
-				<div className="flex flex-wrap items-center justify-center gap-4 pt-4">
-					<figure className="relative group cursor-pointer">
-						<Image src="/glacier.png" alt="Glaciers" width={300} height={300} className="object-cover rounded-lg shadow-md transition-transform duration-300 group-hover:scale-105" />
-						<figcaption className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-60 text-white text-sm p-3 rounded-b-lg opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300">
-							<span>Traversing glaciers in Alaska (I'm on the right!)</span>
-						</figcaption>
-					</figure>
-
-					<figure className="relative group cursor-pointer">
-						<Image src="/rainier.png" alt="Rainier" width={400} height={400} className="object-cover rounded-lg shadow-md transition-transform duration-300 group-hover:scale-105" />
-						<figcaption className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-60 text-white text-sm p-3 rounded-b-lg opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300">
-							<span>Hiking near Mount Rainier, WA!</span>
-						</figcaption>
-					</figure>
-
-					<figure className="relative group cursor-pointer">
-						<Image src="/loon_mountain.png" alt="Loon Mountain" width={400} height={400} className="object-cover rounded-lg shadow-md transition-transform duration-300 group-hover:scale-105" />
-						<figcaption className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-60 text-white text-sm p-3 rounded-b-lg opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300">
-							<span>Photo directly after a trail race up Loon Mountain, NH!</span>
-						</figcaption>
-					</figure>
-
-					<figure className="relative group cursor-pointer">
-						<Image src="/bouldering.png" alt="Bouldering" width={300} height={300} className="object-cover rounded-lg shadow-md transition-transform duration-300 group-hover:scale-105" />
-						<figcaption className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-60 text-white text-sm p-3 rounded-b-lg opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300">
-							<span>Bouldering at local climbing gym!</span>
-						</figcaption>
-					</figure>
-				</div>
+				<Carousel images={images} />
 			</div>
 		</div>
 	);
--- app/components/EducationSection.tsx
+++ app/components/EducationSection.tsx
@@ -9,6 +9,7 @@ const EducationSection = () => {
 			highlights: (
 				<ul>
 					<li>Fall Semester courses: MATH 2210 (Linear Algebra), CS 2110 (Object-Oriented Programming and Data Structures), COGST 2801 (Intro to Game Theory), STS 2810 (Science, Nature, and Knowledge)</li>
+					<li>GPA: 4.00/4.00</li>
 				</ul>
 			)
 		},
@@ -18,8 +19,8 @@ const EducationSection = () => {
 			period: '2021 - 2025',
 			highlights: (
 				<ul>
-					<li>Top 5%, high honors</li>
 					<li>SAT: 1560/1600, PSAT: 1510/1520 (National Merit Finalist)</li>
+					<li>GPA: 4.67/4.00, top 5%</li>
 				</ul>
 			),
 		},
